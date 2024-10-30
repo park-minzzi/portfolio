09fd0449418a834b87f68e93f472304bc6a17c0c
박민지 <112459519+park-minzzi@users.noreply.github.com>
2024-10-30 10:14:06 +0900

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,18 @@
+$(function(){
+
+
+    // 팝업
+    $(".popup-btn").click(function(){
+        $(".popup-view").show();
+    });
+    $(".popup-close").click(function(){
+        $(".popup-view").hide();
+    });
+
+    $(".popup-btn2").click(function(){
+        $(".popup-view2").show();
+    });
+    $(".popup-close2").click(function(){
+        $(".popup-view2").hide();
+    });
+});
